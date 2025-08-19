6a984bb9a99eafc8fba02ec401eb9e1656800fe0
Mahmoudssanad <sanadmahmoud968@gmail.com>
2025-08-19 03:44:45 +0200

    Add: Configuration connection to database

---
+++ Blog_Website/Models/Data/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Blog_Website.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace Blog_Website.Models.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Follow> Follows { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Self relation
+            builder.Entity<Follow>()
+                .HasOne(f => f.Follower)
+                .WithMany(u => u.Followings)
+                .HasForeignKey(x => x.FollowerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Follow>()
+                .HasOne(f => f.Following)
+                .WithMany(u => u.Followers)
+                .HasForeignKey(x => x.FollowingId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Follow>()
+            .HasIndex(f => new { f.FollowerId, f.FollowingId })
+            .IsUnique();
+
+            // one to one relations 
+            builder.Entity<Notification>()
+            .HasOne(n => n.Like)
+            .WithOne(l => l.Notification)
+            .HasForeignKey<Notification>(n => n.LikeId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Notification>()
+            .HasOne(n => n.Comment)
+            .WithOne(l => l.Notification)
+            .HasForeignKey<Notification>(n => n.CommentId)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
--- Blog_Website/Program.cs
+++ Blog_Website/Program.cs
@@ -1,7 +1,12 @@
+using Blog_Website.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("constr")));
 
 var app = builder.Build();
 
