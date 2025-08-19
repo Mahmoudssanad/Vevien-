32b65b9fffbc50815ca8e3017fa3d3c78ebb91ed
Mahmoudssanad <sanadmahmoud968@gmail.com>
2025-08-19 04:41:38 +0200

    Add: BirthdateCustomValidation and AccountViewModel

---
+++ Blog_Website/ViewModel/AccountViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Blog_Website.CustomValidation;
+using Blog_Website.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog_Website.ViewModel
+{
+    public class AccountViewModel
+    {
+        public string UserName { get; set; }
+        public string Address { get; set; }
+
+        [BirthdateValidation]
+        public DateOnly Birthdate { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public GenderEnum Gender { get; set; }
+    }
+}
